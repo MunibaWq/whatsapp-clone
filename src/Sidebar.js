59276124812079ae63d7932355459059a3c3a23b
Muniba Waqar <muniba.wq@gmail.com>
2021-03-17 15:04:56 -0600

    basic page divs added for structure outline, fixed emmet, installed material UI

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,9 +1,24 @@
 import React from "react";
+import "./Sidebar.css";
+
+import { Avatar } from "@material-ui/core";
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import ChatIcon from "@material-ui/icons/Chat";
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
-      <h1>Sidebar</h1>
+      <div className="sidebar__header">
+        <Avatar />
+        <div className="sidebar__headerRight">
+          <DonutLargeIcon />
+          <ChatIcon />
+          <MoreVertIcon />
+        </div>
+      </div>
+      <div className="div sidebar__search"></div>
+      <div className="div sidebar__chats"></div>
     </div>
   );
 };
