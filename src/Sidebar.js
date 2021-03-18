9d886fdbaffe75914920d4332ce3b6a0e9ae4c10
Muniba Waqar <muniba.wq@gmail.com>
2021-03-18 17:48:38 -0600

    adding search bar in sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -5,6 +5,7 @@ import { Avatar, IconButton } from "@material-ui/core";
 import DonutLargeIcon from "@material-ui/icons/DonutLarge";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 import ChatIcon from "@material-ui/icons/Chat";
+import SearchOutlined from "@material-ui/icons/Search";
 
 const Sidebar = () => {
   return (
@@ -23,8 +24,17 @@ const Sidebar = () => {
           </IconButton>
         </div>
       </div>
-      <div className="div sidebar__search"></div>
-      <div className="div sidebar__chats"></div>
+
+      <div className="div sidebar__search">
+        <div className="div sidebar__searchContainer">
+          <SearchOutlined />
+          <input type="text" placeholder="Search or start new chat" />
+        </div>
+      </div>
+
+      <div className="div sidebar__chats">
+        <SidebarChat />
+      </div>
     </div>
   );
 };
